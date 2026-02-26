7ca27772a3e76d1c299e6fc7584667ee107091b3
Nilyok <nilyok.ch@gmail.com>
2026-02-26 12:34:42 -0600

    debug auth execution

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 import JWT_SECRET from "../utils/config.js";
 import { UNAUTHORIZED } from "../utils/errors.js";
 
+console.log("🔥 AUTH FILE LOADED");
+
 export default (req, res, next) => {
+  console.log("🔥 AUTH MIDDLEWARE EXECUTED");
   // if a previous middleware already set req.user (e.g. tests inject a default)
   // skip authentication checks
   if (req.user && req.user._id) {
