b8aad467e1dfd1b9e31dfff5adff640e6c6b8cec
Nilyok <nilyok.ch@gmail.com>
2026-03-11 15:39:52 -0500

    Fix backend authorization and routes: remove Sprint 12 test middleware, protect /items routes, and update sprint.txt to 13

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ import express from "express";
 import usersRouter from "./users.js";
 import itemsRouter from "./clothingItems.js";
 import { login, createUser } from "../controllers/users.js";
+import { getItems } from "../controllers/clothingItems.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
-/* AUTH ROUTES */
+/* Public routes */
 router.post("/signup", createUser);
 router.post("/signin", login);
+router.get("/items", getItems);
 
-/* PUBLIC ROUTES */
+/* Protected routes */
+router.use(auth);
+
 router.use("/users", usersRouter);
 router.use("/items", itemsRouter);
 
